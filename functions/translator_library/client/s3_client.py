f9b5ee66b1a2d27fc7499817b12b1949a885d43d
Mynor Xico <mynor.xico@curantissolutions.com>
2024-06-15 21:23:06 -0600

    Add support for multiple languages and not only two

--- functions/translator_library/client/s3_client.py
+++ functions/translator_library/client/s3_client.py
@@ -1,5 +1,5 @@
 import boto3
-
+import os
 # Create an S3 client
 s3 = boto3.client('s3')
 
@@ -7,7 +7,11 @@ s3 = boto3.client('s3')
 def download_file(s3_bucket, key, local_path):
     # Download the file
     try:
-        s3.download_file(s3_bucket, key, local_path)
-        print(f"File downloaded successfully to {local_path}")
+        file_already_exists = os.path.isfile(local_path)
+        if not file_already_exists:
+            s3.download_file(s3_bucket, key, local_path)
+            print(f"File downloaded successfully to {local_path}")
+        else:
+            print(f"Skipping download of: {local_path} since it already exists")
     except Exception as e:
         print(f"Error downloading file: {e}")
--- functions/translator_library/service/translator_v2.py
+++ functions/translator_library/service/translator_v2.py
@@ -5,6 +5,8 @@ import ctranslate2
 import codecs
 import pyonmttok
 
+import os
+
 sys.path.append('../')
 
 from translator_library.client.s3_client import download_file
@@ -13,58 +15,43 @@ from translator_library.utils.preprocessing import clean
 from translator_library.utils.preprocessing import detokenize
 
 local_file_dir = "/tmp"
+
 bucket_name = get_secret('/traductor-kaqchikel-functions/BUCKET_NAME')
 
-eskaq_model_name = get_secret('/traductor-kaqchikel-functions/ESKAQ_MODEL_NAME')
-kaqes_model_name = get_secret('/traductor-kaqchikel-functions/KAQES_MODEL_NAME')
-
-eskaq_vocab_file = f"{eskaq_model_name}_vocab.txt"
-kaqes_vocab_file = f"{kaqes_model_name}_vocab.txt"
-
-local_eskaq_vocab_file = f"{local_file_dir}/{eskaq_vocab_file}"
-local_kaqes_vocab_file = f"{local_file_dir}/{kaqes_vocab_file}"
+downloaded_models = []
+print("Only first time:", os.listdir(local_file_dir))
+def get_model_name(model):
+    upper_model = model.upper()
+    return get_secret(f"/traductor-kaqchikel-functions/{upper_model}_MODEL_NAME")
 
 
-def init_translator():
-    download_vocabularies()
-    download_models()
-    unzip_models()
+def download_model(model, model_name):
+    print("Downloaded models:", downloaded_models)
+    key = f"{model_name}.zip"
+    local_file_path = f"{local_file_dir}/{key}"
+    download_file(bucket_name, f"{model}/{key}", local_file_path)
+    downloaded_models.append(model_name)
+    return local_file_path
 
 
-def download_vocabularies():
-    download_file(bucket_name, f"eskaq/{eskaq_vocab_file}", local_eskaq_vocab_file)
-    download_file(bucket_name, f"kaqes/{kaqes_vocab_file}", local_kaqes_vocab_file)
-
-def download_models():
-    # eskaq
-    eskaq_key = f"{eskaq_model_name}.zip"
-    eskaq_local_file_path = f"{local_file_dir}/{eskaq_key}"
-    download_file(bucket_name, f"eskaq/{eskaq_key}", eskaq_local_file_path)
-    # kaqes
-    kaqes_key = f"{kaqes_model_name}.zip"
-    kaqes_local_file_path = f"{local_file_dir}/{kaqes_key}"
-    download_file(bucket_name, f"kaqes/{kaqes_key}", kaqes_local_file_path)
-
-def unzip_models():
-    with zipfile.ZipFile(f"{local_file_dir}/{eskaq_model_name}.zip", 'r') as zip_ref:
-        print("Unzipping model")
-        zip_ref.extractall(f"{local_file_dir}/{eskaq_model_name}")
-        print("Model unzipped successfully")
-    with zipfile.ZipFile(f"{local_file_dir}/{kaqes_model_name}.zip", 'r') as zip_ref:
-        print("Unzipping model")
-        zip_ref.extractall(f"{local_file_dir}/{kaqes_model_name}")
-        print("Model unzipped successfully")
+def download_vocabulary(model, model_name):
+    vocab_file_s3_key = f"{model_name}_vocab.txt"
+    vocab_file_local_path = f"{local_file_dir}/{vocab_file_s3_key}"
+    download_file(bucket_name, f"{model}/{vocab_file_s3_key}", vocab_file_local_path)
+    return vocab_file_local_path
 
 
+def unzip_model(model_name):
+    with zipfile.ZipFile(f"{local_file_dir}/{model_name}.zip", "r") as zip_ref:
+        print(f"Unzipping model {model_name}")
+        zip_ref.extractall(f"{local_file_dir}/{model_name}")
+        print(f"Model unzipped successfully {model_name}")
 
 
-
-def get_bpe_text(text, model):
-    vocab_file = local_kaqes_vocab_file if model == 'kaqes' else local_eskaq_vocab_file
-
+def get_bpe_text(text, vocab_file_path):
     tokenizer = pyonmttok.Tokenizer(
         "aggressive",
-        bpe_model_path=vocab_file,
+        bpe_model_path=vocab_file_path,
         joiner_annotate=True,
         segment_numbers=True
     )
@@ -72,20 +59,27 @@ def get_bpe_text(text, model):
     tokenized_text, _ = tokenizer.tokenize(text)
     return ' '.join(tokenized_text)
 
-def get_translation(text, model):
-    model_name = kaqes_model_name if model == 'kaqes' else eskaq_model_name
+
+def get_translation(text, model_name):
     translator = ctranslate2.Translator(f"{local_file_dir}/{model_name}/{model_name}")
     return " ".join(translator.translate_batch([text.split()])[0][0]['tokens'])
 
 
-
-
-
 def translate_text(sentence, model):
-    print("Original text: ", sentence)
+    print("Original text:", sentence)
     preprocessed_sentence = clean(sentence)
-    tokenized_sentence = get_bpe_text(preprocessed_sentence, model)
-    tokenized_translation = get_translation(tokenized_sentence, model)
+
+    model_name = get_model_name(model)
+    # Download vocab
+    vocab_path = download_vocabulary(model, model_name)
+    # Tokenize vocab
+    tokenized_sentence = get_bpe_text(preprocessed_sentence, vocab_path)
+    # Download model
+    download_model(model, model_name)
+    # Unzip model
+    unzip_model(model_name)
+    # Get translation
+    tokenized_translation = get_translation(tokenized_sentence, model_name)
     detokenized_translation = detokenize(tokenized_translation)
     print("Translated text: ", detokenized_translation)
     return detokenized_translation
