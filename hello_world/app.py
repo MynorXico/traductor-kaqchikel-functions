1b7241571680cdc7ed00b8467b20f16ee1e1e21e
Mynor Xico <mynor.xico@curantissolutions.com>
2024-06-16 21:43:39 -0600

    Migrate to containers due to lambda size

---
+++ hello_world/app.py
@@ -0,0 +1,22 @@
+import json
+
+from lib.service.translator_v2 import translate_text
+
+def lambda_handler(event, context):
+    print("Lambda handler:")
+    query = event.get('queryStringParameters', {}).get('query')
+    model = event.get('queryStringParameters', {}).get('model')
+    print("Query: ", query)
+    print("Model:", model)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",  # Adjust this based on your requirements
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",  # Adjust this based on your allowed methods
+        },
+        "body": json.dumps({
+            "translation": translate_text(query, model),
+            "source": query
+        }),
+    }
